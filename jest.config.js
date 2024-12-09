bcd7d84d734be5177ac54e8f6c1a6c44883097ba
janiru-swivel <janiru@swivelgroup.com.au>
2024-12-09 13:01:19 +0530

    cors error fixed

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,9 @@
 export default {
+  transform: {},
   testEnvironment: "node",
+  moduleFileExtensions: ["js"],
+  testMatch: ["**/__tests__/**/*.js", "**/?(*.)+(spec|test).js"],
+  transformIgnorePatterns: ["node_modules/"],
+  collectCoverageFrom: ["src/**/*.{js,jsx}"],
+  coverageDirectory: "coverage",
 };

52a66920347662c7013b177d84770a81b7e274c5
janiru-swivel <janiru@swivelgroup.com.au>
2024-12-10 20:51:44 +0530

    commit for url change frontend

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ mongoose
 const corsOptions = {
   origin: [
     "http://localhost:3000",
-    "https://ignite-employee-system-frontend.vercel.app/",
+    "https://ignite-employee-system-frontend-3jaemuuqe.vercel.app/",
   ], // Allow these origins
   methods: ["*"], // Allow these methods
   allowedHeaders: ["Content-Type", "Authorization"], // Allow these headers
